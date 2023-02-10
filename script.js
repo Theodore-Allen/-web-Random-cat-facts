a598dd3104252cd218ea00bce5668089d3ae6fb4
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-02-09 17:22:13 -0800

    edupdat

--- script.js
+++ script.js
@@ -2,16 +2,14 @@ const factBtn = document.getElementById('factBtn');
 const factTxt = document.getElementById('factTxt');
 const catImg = document.getElementById('img');
 const hart = document.getElementById('hart');
+const body = document.getElementsByName('body');
 async function catPic() {
   hart.classList.remove('hide');
   const endpoint = ' https://api.thecatapi.com/v1/images/search';
   const server = await fetch(endpoint);
   const response = await server.json();
   catImg.style.backgroundImage = `url(${response[0].url})`;
-
-  catimg.addEventListener('onLoad', () => {
-    hart.classList.add('hide');
-  });
+  await re();
 }
 async function catFact() {
   const endpoint = 'https://catfact.ninja/fact';
@@ -24,4 +22,13 @@ catFact();
 factBtn.addEventListener('click', () => {
   catPic();
   catFact();
+  
 });
+catImg.onload = function () {
+  console.log('done');
+  //  hart.classList.add('hide')
+};
+function re() {
+  console.log('loaded');
+  hart.classList.add('hide');
+}
