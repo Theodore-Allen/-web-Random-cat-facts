6fb010fbb82c61d6f174c24c99feeca740302bd3
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-04-13 11:45:50 -0700

    1 file modified

--- MiniGame.js
+++ MiniGame.js
@@ -1,13 +1,17 @@
 const catImg = document.getElementById('img');
 const catSound = new Audio('http://soundbible.com/grab.php?id=1287&type=mp3');
+const cats = []
+
+
+getCatArray().then(e => console.log(e))
+
 
 async function getCatArray() {
-   const endpoint = ' https://api.thecatapi.com/v1/images/search?limit=10';
-  const server = await fetch(endpoint);
-  const response = await server.json();
-  console.log(response);
+  const endpoint = ' https://api.thecatapi.com/v1/images/search?limit=10';
+ const server = await fetch(endpoint);
+ const response = await server.json();
+ 
+ return response;
 }
-
-console
 
 
