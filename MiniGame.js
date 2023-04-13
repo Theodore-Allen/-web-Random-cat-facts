c3b03e36a2a8b1c4659aae87ea8ea5f90abe5638
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-04-13 11:40:19 -0700

    2 files modified

--- MiniGame.js
+++ MiniGame.js
@@ -1,25 +1,13 @@
-const factBtn = document.getElementById('factBtn');
-const factTxt = document.getElementById('factTxt');
 const catImg = document.getElementById('img');
-const hart = document.getElementById('hart');
 const catSound = new Audio('http://soundbible.com/grab.php?id=1287&type=mp3');
-const body = document.getElementsByName('body');
-async function catPic() {
-  hart.classList.remove('hide');
-  const endpoint = '  https://api.thecatapi.com/v1/images/search?limit=10';
+
+async function getCatArray() {
+   const endpoint = ' https://api.thecatapi.com/v1/images/search?limit=10';
   const server = await fetch(endpoint);
   const response = await server.json();
-  
-  catImg.style.backgroundImage = `url(${response[0].url})`;
-  console.log(response)
-  await re();
+  console.log(response);
 }
 
-catPic();
+console
 
 
-
-function re() {
-  console.log('loaded');
-  hart.classList.add('hide');
-}
