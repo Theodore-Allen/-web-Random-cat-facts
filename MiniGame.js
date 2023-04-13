ad4ddc120d3878a8ab5699bfad2725ce713773e2
Theodore-Allen <124205027+Theodore-Allen@users.noreply.github.com>
2023-04-13 13:01:59 -0700

    1 file modified

--- MiniGame.js
+++ MiniGame.js
@@ -1,17 +1,44 @@
 const catImg = document.getElementById('img');
-const catSound = new Audio('http://soundbible.com/grab.php?id=1287&type=mp3');
-const cats = []
+const optionAmount = 4;
+app();
+async function app() {
+  getCatArray().then((e) =>
+    id2CatInfo(
+      e.map((e) => {
+        return e.id;
+      })
+    ).then((e) => {
+      var tempCat = [];
+      for (var i = 0; i < optionAmount; i++) {
+       
+        tempCat.push({name: e[i].breeds[0].name, id: e[i].id, url: e[i].url })
+      }
+      console.log(tempCat)
 
-
-getCatArray().then(e => console.log(e))
-
-
-async function getCatArray() {
-  const endpoint = ' https://api.thecatapi.com/v1/images/search?limit=10';
- const server = await fetch(endpoint);
- const response = await server.json();
- 
- return response;
+    })
+  );
 }
 
+async function getCatArray() {
+  const endpoint =
+    'https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1';
+  const server = await fetch(endpoint);
+  const response = await server.json();
 
+  return response;
+}
+
+async function id2CatInfo(id) {
+  var cats = [];
+  for (var i = 0; i < optionAmount; i++) {
+    const endpoint = 'https://api.thecatapi.com/v1/images/' + id[i];
+    const server = await fetch(endpoint);
+    const response = await server.json();
+    cats.push(response);
+  }
+
+  return cats;
+}
+
+//Api's used
+// https://thecatapi.com/
